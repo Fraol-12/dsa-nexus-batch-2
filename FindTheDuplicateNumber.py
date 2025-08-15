dabdebfb4ae87aa15d30ff0ee20da7d3085afa6c
Fraol_M <fraolm41@gmail.com>
2025-08-15 17:12:37 +0300

    Create FindTheDuplicateNumber.py

---
+++ FindTheDuplicateNumber.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow , fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if fast == slow:
+                break
+
+        slow = 0
+        
+        while fast != slow:
+            slow = nums[slow]
+            fast = nums[fast]
+            if fast == slow:
+               return slow
